0b36bcee9c3fb6c31eef469c22224d90cc8320f3
TijsDelvaux <Tijs.delvaux@gmail.com>
2015-03-16 00:44:34 +0100

    Initial commit

---
+++ app/src/main/java/com/mycompany/myfirstindoorsapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.mycompany.myfirstindoorsapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import com.mycompany.myfirstindoorsapp.ImageTargets.*;
+/**
+ * Sample Android project, powered by indoo.rs :)
+ * 
+ * @author indoo.rs | Philipp Koenig
+ * 
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+    }
+    
+    public void startPlay(View view) {
+    	startActivity(new Intent(this, ImageTargets.class));
+    }
+    
+    public void startMap(View view) {
+    	startActivity(new Intent(this, MapActivity.class));
+    }
+}
