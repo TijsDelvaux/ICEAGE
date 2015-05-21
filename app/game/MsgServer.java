db88ff2e9758b22e04bc884902464c99c040e8b2
TijsDelvaux <Tijs.delvaux@gmail.com>
2015-05-21 16:09:17 +0200

    Ready for anyone to use.

---
+++ app/game/MsgServer.java
@@ -0,0 +1,14 @@
+
+/**
+ * A client sends messages to the server in this format:
+ *      userName + ":" + teamName + ":" + code + ":" + message
+ * where the code is one of the following:
+ */
+public enum MsgServer {
+    DEFAULT,
+    ACORN_REQUEST,
+    ACORN_PICKUP,
+    ENTER_ZONE,
+    SET_TRAP,
+    REGISTER
+}
